ad46c68f9dc0c3567d55694567a9c125b263f74c
Sam Hemelryk <sam@moodle.com>
2012-05-22 15:14:59 +1200

    Fixed up version.php requires number

--- version.php
+++ version.php
@@ -7,6 +7,7 @@
  */
 
 $plugin->version = 2012011000;
+$plugin->release = '0.3.0 (beta)';
 $plugin->component = 'block_css_theme_tool';
-$plugin->requires = 2012020200.00; // Moodle 2.2 release
+$plugin->requires = 2011120500.00; // Moodle 2.2 release
 $plugin->maturity = MATURITY_BETA;
