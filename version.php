015ab8d09ceebf027b163b477bb2a970ae5720b3
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-01-25 10:16:24 +0000

    Fixing the 'does not define the standard capability... addinstance' issue, and version bump.

--- version.php
+++ version.php
@@ -6,7 +6,7 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2012011000;
+$plugin->version = 2013012500;
 $plugin->release = '0.3.0 (beta)';
 $plugin->component = 'block_css_theme_tool';
 $plugin->requires = 2011120500.00; // Moodle 2.2 release
